53e934eca9feccd12fb4add473d11a73626f3eed
Patrick Veilleux <patrix87@gmail.com>
2025-03-02 21:51:03 -0500

    initial commit

---
+++ ITR2-Unbound/scripts/fun.lua
@@ -0,0 +1,250 @@
+local fun = {}
+
+function fun:ToggleDebugMode()
+    debugmode = not debugmode
+    if debugmode then
+        print("Debug mode enabled")
+    else
+        print("Debug mode disabled")
+    end
+end
+
+---@param hunger number
+function fun:SetHunger(hunger)
+    local PlayerStatsComponent = FindFirstOf("PlayerStatsComponent")
+    if (PlayerStatsComponent) then
+        local oldHunger = PlayerStatsComponent:GetHunger()
+        if debugmode then print("Current Hunger: " .. tostring(oldHunger)) end
+        PlayerStatsComponent.ChangeHungerAndNotifyAll(hunger)
+        local newHunger = PlayerStatsComponent:GetHunger()
+        if debugmode then print("New Hunger: " .. tostring(PlayerStatsComponent:GetHunger())) end
+    else
+        if debugmode then print("Waiting for PlayerStatsComponent to be available.") end
+    end
+end
+
+---@param stamina number
+function fun:SetStamina(stamina)
+    local PlayerStatsComponent = FindFirstOf("PlayerStatsComponent")
+    if (PlayerStatsComponent) then
+        local oldStamina = PlayerStatsComponent:GetStamina()
+        if debugmode then print("Current Stamina: " .. tostring(oldStamina)) end
+        PlayerStatsComponent.ChangeStaminaAndNotifyAll(stamina)
+        local newStamina = PlayerStatsComponent:GetStamina()
+        if debugmode then print("New Stamina: " .. tostring(newStamina)) end
+    else
+        if debugmode then print("Waiting for PlayerStatsComponent to be available.") end
+    end
+end
+
+---@param health number
+function fun:SetHealth(health)
+    local PlayerStatsComponent = FindFirstOf("PlayerStatsComponent")
+    if (PlayerStatsComponent) then
+        local oldHealth = PlayerStatsComponent:GetHealth()
+        if debugmode then print("Current Health: " .. tostring(oldHealth)) end
+        PlayerStatsComponent.CurrentHealth = health
+        local newHealth = PlayerStatsComponent:GetHealth()
+        if debugmode then print("New Health: " .. tostring(newHealth)) end
+    else
+        if debugmode then print("Waiting for PlayerStatsComponent to be available.") end
+    end
+end
+
+---@param amount number
+function fun:AddMoney(amount)
+    --- Option 1
+    -- local RadiusGameDataSubsystem = FindFirstOf("RadiusGameDataSubsystem")
+    -- RadiusGameDataSubsystem:AddMoney(amount, "", 0)
+    --- Option 2
+    local RadiusGameDataReplicator = FindFirstOf("RadiusGameDataReplicator")
+    if (RadiusGameDataReplicator) then
+        local oldMoney = RadiusGameDataReplicator.Money
+        if debugmode then print("Current Money: " .. tostring(oldMoney)) end
+        RadiusGameDataReplicator.money = (oldMoney + amount) 
+        local newMoney = RadiusGameDataReplicator.Money
+        if debugmode then print("New Money: " .. tostring(newMoney)) end
+    else 
+        if debugmode then print("Waiting for RadiusGameDataReplicator to be available.") end
+    end
+end
+
+---@param accessLevel integer
+function fun:SetAccessLevel(accessLevel)
+    local RadiusGameDataReplicator = FindFirstOf("RadiusGameDataReplicator")
+    if (RadiusGameDataReplicator) then 
+        local oldAccessLevel = RadiusGameDataReplicator:GetAccessLevel()
+        if debugmode then print("Current Access Level: " .. tostring(oldAccessLevel)) end
+        RadiusGameDataReplicator:SetAccessLevel(accessLevel)
+        local newAccessLevel = RadiusGameDataReplicator:GetAccessLevel()
+        if debugmode then print("New Access Level: " .. tostring(newAccessLevel)) end
+    else 
+        if debugmode then print("Waiting for RadiusGameDataReplicator to be available.") end
+    end
+end
+
+function fun:ToggleNoClip()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    if (RadiusCheatSubsystem) then
+        local oldNoClip = RadiusCheatSubsystem:GetNoClip()
+        RadiusCheatSubsystem:SetNoClip(not oldNoClip)
+        local newNoClip = RadiusCheatSubsystem:GetNoClip()
+        print("NoClip: " .. tostring(newNoClip))
+    else
+        if debugmode then print("Waiting for RadiusCheatSubsystem to be available.") end
+    end
+end
+
+--- TO BE TESTED ---
+
+--[[
+Levels:
+Level.Radius.Forest.Gate2
+Level.Radius.Peninsula.Gate2
+Level.Radius.Town
+Level.Radius.Town.Gate
+Level.Test.QA.Gate
+Level.Test.TownBAC
+Level.Test.WaterTest
+Level.Test.WaterTest2
+]] 
+
+-- function testMethod()
+--     local UFLGeneral = FindFirstOf("UFLGeneral")
+--     local map = UFLGeneral:RequestGameplayTag("Level.Radius.Peninsula.Gate2")
+--     print(map:ToString())
+-- end
+
+-- function fun:testMethod()
+--     local Object = "UFLGeneral"
+--     local Instance = FindFirstOf(Object)
+--     if not Instance:IsValid() then
+--         print("No Instance of class: " .. Object .. " found.")
+--     else 
+--         print("Instance of class: " .. Object .. " found.")
+--         print(toString(Instance))
+--         local map = UFLGeneral:RequestGameplayTag("Level.Radius.Peninsula.Gate2")
+--         print(map:ToString())
+--     end
+-- end
+
+-- function fun:testMethod()
+--     local Object = "CheatPanel"
+--     local Instance = FindFirstOf(Object)
+--     if not Instance:IsValid() then
+--         print("No Instance of class: " .. Object .. " found.")
+--     else 
+--         print("Instance of class: " .. Object .. " found.")
+--         print(toString(Instance))
+--     end
+-- end
+
+-- function fun:TravelToPeninsula()
+--     local RadiusLevelSubsystem = FindFirstOf("RadiusLevelSubsystem")
+--     local levelName = RadiusLevelSubsystem:GetCurrentLevelName()
+--     local levelTag = RadiusLevelSubsystem:GetCurrentLevel()
+--     if debugmode then print("Current Level: " .. levelName:ToString()) end
+--     print(tostring(levelTag))
+--     RadiusLevelSubsystem:ValidateLevelTag(levelTag)
+--     print(tostring(RadiusLevelSubsystem:IsLevelAvailable(levelTag)))
+--     RadiusLevelSubsystem:LoadLevel(levelTag,true,true,true) -- Crash the game
+-- end
+
+--- THESE DO NOTHING AS FAR AS I CAN TELL ---
+
+function fun:ToggleAiDebug()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    local aiDebug = RadiusCheatSubsystem:IsAIDebug()
+    RadiusCheatSubsystem:SetAIDebug(not aiDebug)
+    local aiDebug = RadiusCheatSubsystem:IsAIDebug()
+    print("AiDebug: " .. tostring(aiDebug))
+end
+
+function fun:SpawnMissionArtefacts()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    RadiusCheatSubsystem:SpawnMissionArtefacts()
+end
+
+function fun:ToggleControllerVisibility()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    RadiusCheatSubsystem:ToggleControllerVisibility()
+end
+
+function fun:SpawnStartingGear()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    RadiusCheatSubsystem:SpawnStartingGear()
+end
+
+---@param value number
+function fun:SetSpeedCheatValue(value)
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    RadiusCheatSubsystem:SetSpeedCheatValue(value)
+    local speedCheatValue = RadiusCheatSubsystem:GetSpeedCheatValue()
+    print("SpeedCheatValue: " .. tostring(speedCheatValue))
+end
+
+function fun:ToggleSpeedCheat()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    local speedCheat = RadiusCheatSubsystem:IsSpeedCheat()
+    RadiusCheatSubsystem:SetSpeedCheat(not speedCheat)
+    local speedCheat = RadiusCheatSubsystem:IsSpeedCheat()
+    print("SpeedCheat: " .. tostring(speedCheat))
+end
+
+function fun:ToggleShowPlayerName()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    local showPlayerName = RadiusCheatSubsystem:IsShowPlayerName()
+    RadiusCheatSubsystem:SetShowPlayerName(not showPlayerName)
+    local showPlayerName = RadiusCheatSubsystem:IsShowPlayerName()
+    print("ShowPlayerName: " .. tostring(showPlayerName))
+end
+
+function fun:ToggleMagReloadCheat()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    local magReloadCheat = RadiusCheatSubsystem:IsMagReloadCheat()
+    RadiusCheatSubsystem:SetMagReloadCheat(not magReloadCheat)
+    local magReloadCheat = RadiusCheatSubsystem:IsMagReloadCheat()
+    print("MagReloadCheat: " .. tostring(magReloadCheat))
+end
+
+function fun:ToggleJumpAllowed()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    local jumpAllowed = RadiusCheatSubsystem:IsJumpAllowed()
+    RadiusCheatSubsystem:SetJumpAllowed(not jumpAllowed)
+    local jumpAllowed = RadiusCheatSubsystem:IsJumpAllowed()
+    print("JumpAllowed: " .. tostring(jumpAllowed))
+end
+
+function fun:ToggleInvisibleMode()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    local invisibleMode = RadiusCheatSubsystem:IsInvisibleMode()
+    RadiusCheatSubsystem:SetInvisibleMode(not invisibleMode)
+    local invisibleMode = RadiusCheatSubsystem:IsInvisibleMode()
+    print("InvisibleMode: " .. tostring(invisibleMode))
+end
+
+function fun:ToggleInfiniteAmmoCheat()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    local infiniteAmmoCheat = RadiusCheatSubsystem:IsInfiniteAmmoCheat()
+    RadiusCheatSubsystem:SetInfiniteAmmoCheat(not infiniteAmmoCheat)
+    local infiniteAmmoCheat = RadiusCheatSubsystem:IsInfiniteAmmoCheat()
+    print("InfiniteAmmoCheat: " .. tostring(infiniteAmmoCheat))
+end
+
+function fun:ToggleImmortalMode()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    local immortalMode = RadiusCheatSubsystem:IsImmortalMode()
+    RadiusCheatSubsystem:SetImmortalMode(not immortalMode)
+    local immortalMode = RadiusCheatSubsystem:IsImmortalMode()
+    print("ImmortalMode: " .. tostring(immortalMode))
+end
+
+function fun:ToggleGodMode()
+    local RadiusCheatSubsystem = FindFirstOf("RadiusCheatSubsystem")
+    local godMode = RadiusCheatSubsystem:IsGodMode()
+    RadiusCheatSubsystem:SetGodMode(not godMode)
+    local godMode = RadiusCheatSubsystem:IsGodMode()
+    print("GodMode: " .. tostring(godMode))
+end
+
+return fun
---
+++ ITR2-Unbound/scripts/main.lua
@@ -0,0 +1,189 @@
+local UEHelpers = require("UEHelpers")
+local fun = require("fun")
+
+print("ITR2-Unbound started")
+
+debugmode = false
+if debugmode then
+    print("Debug mode enabled")
+end
+
+local infiniteStamina = false
+local infiniteHealth = false
+local noHunger = false
+
+RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:OnLevelLoaded", function()
+    ExecuteWithDelay(1000, function()
+        LoopAsync(500, function()
+            if (infiniteStamina) then
+                fun:SetStamina(100)
+                fun:SetHunger(10000)
+            end
+            if (infiniteHealth) then
+                fun:SetHealth(100)
+            end
+            if (noHunger) then
+                fun:SetHunger(10000)
+            end
+            return false -- Loops forever
+        end)
+    end)
+end)
+
+RegisterKeyBind(Key.D, function()
+    fun:ToggleDebugMode()
+end)
+
+RegisterKeyBind(Key.ZERO, function()
+    fun:SetAccessLevel(0)
+end)
+
+RegisterKeyBind(Key.ONE, function()
+    fun:SetAccessLevel(1)
+end)
+
+RegisterKeyBind(Key.TWO, function()
+    fun:SetAccessLevel(2)
+end)
+
+RegisterKeyBind(Key.THREE, function()
+    fun:SetAccessLevel(3)
+end)
+
+RegisterKeyBind(Key.M, function()
+    fun:AddMoney(10000)
+end)
+
+RegisterKeyBind(Key.C, function()
+    fun:ToggleNoClip()
+end)
+
+RegisterKeyBind(Key.S, function()
+    infiniteStamina = not infiniteStamina
+    if infiniteStamina then
+        if debugmode then print("Infinite Stamina enabled") end
+    else
+        if debugmode then print("Infinite Stamina disabled") end
+    end
+end)
+
+RegisterKeyBind(Key.L, function()
+    infiniteHealth = not infiniteHealth
+    if infiniteHealth then
+        if debugmode then print("Infinite Health enabled") end
+    else
+        if debugmode then print("Infinite Health disabled") end
+    end
+end)
+
+RegisterKeyBind(Key.H, function()
+    noHunger = not noHunger
+    if noHunger then
+        if debugmode then print("No Hunger enabled") end
+    else
+        if debugmode then print("No Hunger disabled") end
+    end
+end)
+
+RegisterKeyBind(Key.J, function()
+    fun:ToggleJumpAllowed()
+end)
+
+
+--[[
+
+THESE DON'T ACTUALLY DO ANYTHING
+
+RegisterKeyBind(Key.A, function()
+    fun:ToggleAiDebug()
+end)
+
+RegisterKeyBind(Key.N, function()
+    fun:SpawnMissionArtefacts()
+end)
+
+RegisterKeyBind(Key.V, function()
+    fun:ToggleControllerVisibility()
+end)
+
+RegisterKeyBind(Key.H, function()
+    fun:SpawnStartingGear()
+end)
+
+RegisterKeyBind(Key.NUM_ONE, function()
+    fun:SetSpeedCheatValue(1)
+end)
+
+RegisterKeyBind(Key.NUM_TWO, function()
+    fun:SetSpeedCheatValue(2)
+end)
+
+RegisterKeyBind(Key.NUM_THREE, function()
+    fun:SetSpeedCheatValue(3)
+end)
+
+RegisterKeyBind(Key.NUM_FOUR, function()
+    fun:SetSpeedCheatValue(4)
+end)
+
+RegisterKeyBind(Key.NUM_FIVE, function()
+    fun:SetSpeedCheatValue(5)
+end)
+
+RegisterKeyBind(Key.NUM_SIX, function()
+    fun:SetSpeedCheatValue(6)
+end)
+
+RegisterKeyBind(Key.NUM_SEVEN, function()
+    fun:SetSpeedCheatValue(7)
+end)
+
+RegisterKeyBind(Key.NUM_EIGHT, function()
+    fun:SetSpeedCheatValue(8)
+end)
+
+RegisterKeyBind(Key.NUM_NINE, function()
+    fun:SetSpeedCheatValue(9)
+end)
+
+RegisterKeyBind(Key.NUM_ZERO, function()
+    fun:SetSpeedCheatValue(10)
+end)
+
+RegisterKeyBind(Key.F, function()
+    fun:ToggleSpeedCheat()
+end)
+
+RegisterKeyBind(Key.P, function()
+    fun:ToggleShowPlayerName()
+end)
+
+RegisterKeyBind(Key.R, function()
+    fun:ToggleMagReloadCheat()
+end)
+
+RegisterKeyBind(Key.V, function()
+    fun:ToggleInvisibleMode()
+end)
+
+RegisterKeyBind(Key.A, function()
+    fun:ToggleInfiniteAmmoCheat()
+end)
+
+RegisterKeyBind(Key.I, function()
+    fun:ToggleImmortalMode()
+end)
+
+RegisterKeyBind(Key.G, function()
+    fun:ToggleGodMode()
+end)
+
+]]
+
+-- RegisterKeyBind(Key.X, function()
+--     fun:testMethod()
+-- end)
+
+-- RegisterKeyBind(Key.T, function()
+--     fun:TravelToPeninsula()
+-- end)
